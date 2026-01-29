a662b092f44ba12df3012a93c2b3d02c1462a692
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-29 00:13:44 +0000

    Add files via upload

--- unpack.py
+++ unpack.py
@@ -1,5 +1,16 @@
-import os
-import struct
+def unpacks(r):
+    try:
+        rr=0
+        counter=0
+        for n in r:
+            n:int=n
+            rr:int=rr+n*num[counter]
+            counter=counter+1
+        
+        return rr
+    except:
+        return 0
+
 print( "\033c\033[40;37m\n ? ")
 a="my.dat"
 b=[]
@@ -7,15 +18,11 @@ num=[1,256,256*256,256*256*256]
 f1=open(a,"rb")
 while(1):
     try:
-        rr=0
+        
         r=f1.read(4)
         if len(r)<4:
             break
-        counter=0
-        for n in r:
-            n:int=n
-            rr:int=rr+n*num[counter]
-            counter=counter+1
+        rr=unpacks(r)
         b=b+[rr]
     except:
         break
